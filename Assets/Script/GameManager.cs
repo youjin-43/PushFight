4b8ded04591ccb2eae8944e62a173f9e52b1d2dc
youjin43 <wlwlfdl3324@naver.com>
2024-11-04 06:27:17 +0900

    시간에 따른 state분리 중
    state별 시간 간격? 만 조정하면 될것 같음!!!!

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -29,46 +29,63 @@ public class GameManager : MonoBehaviour
 
     public enum State
     {
-        Day,
-        Night,
+        Day, // 0.7 ~ 0.1
+        SunSet, // 0.1 ~ 0.2
+        Night, // 0.3 ~ 0.6
+        Twilight, // 0.6 ~ 0.7
         GameOver
     }
 
 
     public State GameState = State.Day;
-    
+
+    [Header("TimeState Control")]
+    public float Daytime = 20;
     public float SkyScrollSpeed = 5f;
+    float Offset_DayStart = 0.7f;
+    [SerializeField] Renderer SkyRend;
+    [SerializeField] float offset;
 
     void Start()
     {
         MapManager.instance.GenTiles();// 맵 생성 
         MapManager.instance.GenEnergy(); // 에너지 맵에 배치
-        ChangeStateToDay(); //게임 상태 낮으로 시작 
     }
-
-    // Update is called once per frame
+   
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        offset = Offset_DayStart + (Time.time * SkyScrollSpeed / 200) % 1f;
+        SkyRend.material.mainTextureOffset = new Vector2(offset, 0);
+
+        if(0.8f<offset && offset <= 0.9)
         {
-            if (GameState == State.Day) {
-                ChangeStateToNight();
-            }
-            else
-            {
-                ChangeStateToDay();
-            }
+            // 저녁
+            if(GameState != State.SunSet) ChangeState_ToSunSet();
+
+        }
+        else if (0.9 < offset && offset <= 1.3)
+        {
+            // 밤 
+            if (GameState != State.Night) ChangeStateToNight();
+        }
+        else if (1.3 < offset && offset <= 1.4)
+        {
+            //황혼 
+            if (GameState != State.Twilight) ChangeState_ToTwilight();
+        }
+        else
+        {
+            //낮 
+            if (GameState != State.Day) ChangeStateToDay();
         }
     }
-    
-    // TODO : 플레이어 전투 애니메이션으로  전환
-    // TODO : 카메라 위치 전환 
-    void ChangeStateToNight()
+
+    /// <summary>
+    /// 5가 디폴트 
+    /// </summary>
+    void SetTimeSpeed(float speed)
     {
-        Debug.Log("밤이 되었습니다~");
-        GameState = State.Night;
-        MapManager.instance.Stop_ItemSpawnRepeatedly();
-        MapManager.instance.Stop_TileScrolling();
+        SkyScrollSpeed = speed;
     }
 
     void ChangeStateToDay()
@@ -78,6 +95,41 @@ public class GameManager : MonoBehaviour
         MapManager.instance.Start_ItemSpawnRepeatedly();
         MapManager.instance.Start_TileScrolling();
     }
+
+
+    /// <summary>
+    /// 저녁 : 아이템 스폰이 멈춤 
+    /// </summary>
+    void ChangeState_ToSunSet()
+    {
+        Debug.Log("저녁이 되었습니다~");
+        GameState = State.SunSet;
+        SetTimeSpeed(2);
+        MapManager.instance.Stop_ItemSpawnRepeatedly();
+    }
+
+
+    /// <summary>
+    /// 밤 : 전투 준비 + 몹 스폰 + 전투 진행
+    /// </summary>
+    
+    // TODO : 캐릭터 달리기 모션 멈추기 
+    void ChangeStateToNight()
+    {
+        Debug.Log("밤이 되었습니다~");
+        GameState = State.Night;
+        SetTimeSpeed(5);
+        MapManager.instance.Stop_TileScrolling();
+    }
+
+    void ChangeState_ToTwilight()
+    {
+        Debug.Log("새벽이 되었습니다~");
+        GameState = State.Twilight;
+    }
+
+
+
 
     void GameOver()
     {
--- Assets/Script/SkyRotater.cs
+++ Assets/Script/SkyRotater.cs
@@ -2,20 +2,29 @@ using UnityEngine;
 
 public class SkyRotater : MonoBehaviour
 {
-    float scrollSpeed = 5f;
-    Renderer rend;
+    //[SerializeField] float scrollSpeed = 5f;
+    //Renderer rend;
 
-    void Start()
-    {
-        rend = GetComponent<Renderer>();
-        scrollSpeed = GameManager.instance.SkyScrollSpeed;
-    }
+    //float Offset_DayStart = 0.7f;
 
-    float offset;
-    void Update()
-    {
-        offset = (Time.time * scrollSpeed / 200)%1f;
-        rend.material.mainTextureOffset = new Vector2(offset, 0);
-        
-    }
+    //void Start()
+    //{
+    //    rend = GetComponent<Renderer>();
+    //    scrollSpeed = GameManager.instance.SkyScrollSpeed;
+    //}
+
+    //float offset;
+    //void Update()
+    //{
+    //    offset = (Offset_DayStart + Time.time * scrollSpeed / 200)%1f;
+    //    rend.material.mainTextureOffset = new Vector2(offset, 0);
+    //}
+
+    ///*
+     
+    //0.7 ~ 0.1 : 낮
+    //0.1 ~ 0.2 : 저녁 
+    //0.3 ~ 0.6 : 밤
+     
+    // */
 }
